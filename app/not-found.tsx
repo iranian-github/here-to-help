aa929498001e2f105cc7724ab5f64db211bdb361
Emad Rad <codewithemad@gmail.com>
2025-01-31 00:36:27 +0330

    style: improved centering and spacing for NotFound

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <main className='container mx-auto px-4 py-8 text-center'>
-      <h1 className='mb-3 text-3xl font-bold'>متاسفانه صفحه‌ای که به دنبال آن هستید وجود ندارد</h1>
+    <main className='container mx-auto flex h-[calc(100vh-200px)] flex-col items-center justify-center px-4'>
+      <h1 className='mb-5 text-3xl font-bold'>متاسفانه صفحه‌ای که به دنبال آن هستید وجود ندارد</h1>
       <Link href='/'>بازگشت به صفحه اصلی</Link>
     </main>
   )
