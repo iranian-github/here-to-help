c70170d9ac560a2d7173283067f8ca757927cd31
Emad Rad <codewithemad@gmail.com>
2025-02-01 00:04:59 +0330

    feat: make header title a clickable link to homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
     <header className='mb-8 mt-12 text-center sm:mb-12 lg:mb-16 lg:mt-32'>
-      <h1 className='mb-3 text-3xl font-bold sm:text-4xl lg:mb-5 lg:text-5xl'>اینجائیم برای کمک</h1>
+      <h1 className='mb-3 text-3xl font-bold sm:text-4xl lg:mb-5 lg:text-5xl'>
+        <Link href='/' className='text-black hover:text-black'>
+          اینجائیم برای کمک
+        </Link>
+      </h1>
       <p className='mx-auto max-w-2xl text-base leading-relaxed text-gray-500 sm:text-lg'>
         لازم نیست به تنهایی از این مسیر عبور کنید.
       </p>
