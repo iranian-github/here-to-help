f32ca5296a00b3399823bdea6f72441cb5b35024
Emad Rad <codewithemad@gmail.com>
2025-03-05 12:50:00 +0330

    feat: add sitemap generation for dynamic routing

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ''
+
+  const routes = [
+    '',
+    '/free-hiv-test-centers',
+    // TODO: We can make this dynamic
+  ]
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: '2025-03-05',
+    changeFrequency: 'daily',
+    priority: route === '' ? 1 : 0.8,
+  }))
+}
