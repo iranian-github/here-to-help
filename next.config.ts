acec550fc3145bb9d0b419f27f37621228533df9
Emad Rad <codewithemad@gmail.com>
2025-03-05 13:13:42 +0330

    feat: add environment variables and set output to export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || '',
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV || 'development'
+  },
+  output: 'export'
 }
 
 export default nextConfig
